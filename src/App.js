3b049c8203e63b5d1f80854ae803fd7a6481491e
= <=>
2020-09-24 16:16:33 +0300

    using arrow function as a component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-
 import "./App.css";
 import Header from "./header";
 import Banner from "./banner";
+import MainText from "./content";
 
 function App() {
   return (
     <div className="content">
       <Header />
       <Banner text="to my first page created with react.js" />
+      <MainText />
     </div>
   );
 }
