dddf9525f611262e7bf3febc6d5b7c1b169600ca
Nare-Stepanyan <narestepanyan90@gmail.com>
2020-11-08 16:40:44 +0300

    creating delete button for tasks

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,7 +1,27 @@
 import React from "react";
 import styles from "./task.module.css";
+import { Button, Card } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
 
 function Task(props) {
-  return <p className={styles.task}>{props.newTask}</p>;
+  return (
+    <Card className={styles.card}>
+      <Card.Body>
+        <input type="checkbox" />
+        <Card.Text className={styles.task}>{props.newTask.text}</Card.Text>
+        <Button className={styles.cardButton} variant="warning">
+          <FontAwesomeIcon icon={faEdit} />
+        </Button>
+        <Button
+          variant="danger"
+          className={styles.cardButton}
+          onClick={() => props.removeTask(props.newTask._id)}>
+          <FontAwesomeIcon icon={faTrash} />
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 }
+
 export default Task;
--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Task from "../Task/Task";
 import styles from "./ToDo.module.css";
+import idGenerator from "./../../helpers/idGenerator";
 import {
   Button,
   Container,
@@ -21,9 +22,27 @@ class ToDo extends Component {
     });
   };
   handleClick = () => {
+    if (!this.state.inputValue) {
+      return;
+    }
+    const newTask = {
+      text: this.state.inputValue,
+      _id: idGenerator(),
+    };
     this.setState({
-      tasks: [...this.state.tasks, this.state.inputValue],
+      tasks: [newTask, ...this.state.tasks],
       inputValue: "",
+    });
+  };
+  handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      this.handleClick();
+    }
+  };
+  removeTask = (id) => {
+    const newTasks = this.state.tasks.filter((task) => task._id !== id);
+    this.setState({
+      tasks: newTasks,
     });
   };
   render() {
@@ -31,37 +50,47 @@ class ToDo extends Component {
     const { tasks } = this.state;
     const newTaskList = tasks.map((el, i) => {
       return (
-        <Col key={i} lg={3} className={styles.taskColumns}>
-          <Task newTask={el} />
+        <Col key={el._id} xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Task newTask={el} removeTask={this.removeTask} />
         </Col>
       );
     });
 
     return (
       <>
-        <Container>
-          <Row className="no-gutters">
-            <h1 className={styles.title}>To Do List</h1>
-          </Row>
-          <Row className="no-gutters">
-            <InputGroup>
-              <FormControl
-                placeholder="Add new task"
-                aria-label="Add new task"
-                aria-describedby="basic-addon2"
-                onChange={this.handleChange}
-                value={inputValue}
-                type="text"
-              />
-              <InputGroup.Append>
-                <Button variant="dark" onClick={this.handleClick}>
-                  Add Task
-                </Button>
-              </InputGroup.Append>
-            </InputGroup>
-          </Row>
-          <Row className="no-gutters">{newTaskList}</Row>
-        </Container>
+        <div>
+          <Container>
+            <Row>
+              <Col>
+                <h1 className={styles.title}>My Tasks</h1>
+              </Col>
+            </Row>
+            <Row className="justify-content-center">
+              <Col lg={6} xs={12} sm={10} md={8}>
+                <InputGroup>
+                  <FormControl
+                    placeholder="Input your new task"
+                    aria-label="Input your new task"
+                    aria-describedby="basic-addon2"
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleKeyDown}
+                    value={inputValue}
+                    type="text"
+                  />
+                  <InputGroup.Append>
+                    <Button
+                      className={styles.addButton}
+                      onClick={this.handleClick}
+                      disabled={!inputValue}>
+                      Add Task
+                    </Button>
+                  </InputGroup.Append>
+                </InputGroup>
+              </Col>
+            </Row>
+            <Row>{newTaskList}</Row>
+          </Container>
+        </div>
       </>
     );
   }
