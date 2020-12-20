d1648e454ae898ae33486776e76b6a2f479d0c1e
Nare-Stepanyan <narestepanyan90@gmail.com>
2020-12-20 11:56:39 +0300

    connecting components to redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+//import App from "./App";
+import Counter from "./components/AppPages/Counter/Counter";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const defaultState = {
+  count: 0,
+};
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "CHANGE_VALUE":
+      return {
+        count: state.count + action.value,
+      };
+    case "RESET_VALUE":
+      return { count: 0 };
+
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        {/* <App /> */}
+        <Counter />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
