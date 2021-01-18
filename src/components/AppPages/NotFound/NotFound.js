1eda270152641c7ce79b85859562bd2bd089f670
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-01-18 17:42:53 +0300

    added some styles

--- src/components/AppPages/NotFound/NotFound.js
+++ src/components/AppPages/NotFound/NotFound.js
@@ -1,17 +1,28 @@
-import React from "react";
 import styles from "./NotFound.module.css";
 import { Link } from "react-router-dom";
 import error from "./../../../assets/images/error.png";
-
+import React, { useState, useEffect } from "react";
+import Spinner from "./../../Spinner/Spinner";
 export default function NotFound() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.errorText}>
-        <img src={error} alt="not found" />
-        <p className={styles.link}>
-          <Link to="/">go home</Link>
-        </p>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className={styles.wrapper}>
+          <div className={styles.errorText}>
+            <img src={error} alt="not found" />
+            <p className={styles.link}>
+              <Link to="/">go home</Link>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
